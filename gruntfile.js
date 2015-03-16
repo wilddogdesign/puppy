71a49044f7b7f40ffaeb648bf137f1002ea3ce9b
Michael Rumbesht <m.rumbesht@gmail.com>
2015-03-16 01:48:10 +0000

    Add Bower auto-include, customizable minification, libsass with sourcemaps

--- gruntfile.js
+++ gruntfile.js
@@ -1,20 +1,58 @@
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
 module.exports = function(grunt) {
 
   // REQUIRE
   require('time-grunt')(grunt);
-  require('load-grunt-tasks')(grunt);
+  // require('load-grunt-tasks')(grunt);
+  require('jit-grunt')(grunt, {
+    /* jshint camelcase:false */
+    useminPrepare: 'grunt-usemin'
+    /* jshint camelcase:true */
+  });
+
 
   // CONFIG
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    'http-server': {
-      dev: {
-        root: "dist",
+    // 'http-server': {
+    //   dev: {
+    //     root: "dist",
+    //     port: 8888,
+    //     host: "127.0.0.1",
+    //     runInBackground: true
+    //   }
+    // },
+
+    connect: {
+      options: {
         port: 8888,
-        host: "127.0.0.1",
-        runInBackground: true
-      }
+        hostname: 'localhost',
+        open: true,
+        livereload: 35729,
+        useAvailablePort: true
+      },
+      server: {
+        options: {
+          base: 'dist',
+          livereload: true
+        }
+      },
+      livereload: {
+        options: {
+          middleware: function(connect) {
+            return [
+              mountFolder( connect, 'dist' ),
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect().use('/src/css', connect.static('./src/css')),
+              mountFolder( connect, 'src' )
+            ];
+          }
+        }
+      },
     },
 
     liquid: {
@@ -59,6 +97,37 @@ module.exports = function(grunt) {
       },
     },
 
+    // Ruby SASS
+    // sass: {
+    //   dev: {
+    //     options: {
+    //       outputStyle: 'nested',
+    //       sourceMap: true
+    //     },
+    //     files: [{
+    //       expand: true,
+    //       cwd: 'src/css',
+    //       src: '*.scss',
+    //       dest: 'dist/assets/css',
+    //       ext: '.css'
+    //     }]
+    //   },
+    //   prod: {
+    //     options: {
+    //       outputStyle: 'compressed',
+    //       sourceMap: false
+    //     },
+    //     files: [{
+    //       expand: true,
+    //       cwd: 'src/css',
+    //       src: '*.scss',
+    //       dest: 'dist/assets/css',
+    //       ext: '.css'
+    //     }]
+    //   }
+    // },
+
+    // LibSass
     sass: {
       dev: {
         options: {
@@ -88,17 +157,63 @@ module.exports = function(grunt) {
       }
     },
 
-    uglify: {
-      src: {
-        files: [{
-            expand: true,
-            cwd: 'src/js',
-            src: 'app.js',
-            dest: 'dist/assets/js',
-            ext: '.js'
-        }]
+    autoprefixer: {
+      options: {
+        browsers: ['> 1%', 'last 2 versions', 'ie 9']
+      },
+      dev: {
+        options: {
+          map: true
+        },
+        expand: true,
+        cwd:  'dist',
+        src:  'assets/css/*.css',
+        dest: 'dist'
+      },
+      prod: {
+        expand: true,
+        cwd:  'dist',
+        src:  'assets/css/*.css',
+        dest: 'dist',
+      },
+    },
+
+    // Automatic Bower script and styles inclusion
+    wiredep: {
+
+      main: {
+
+        exclude: [
+          'bower_components/modernizr/modernizr.js'
+        ],
+
+        // Point to the files that should be updated when
+        // you run `grunt wiredep`
+        src: [
+          'dist/**/*.{html,php}'
+          // 'src/css/main.scss'
+        ],
+
+        options: {
+          // See wiredep's configuration documentation for the options
+          // you may pass:
+
+          // https://github.com/taptapship/wiredep#configuration
+        }
       }
     },
+
+    // uglify: {
+    //   src: {
+    //     files: [{
+    //         expand: true,
+    //         cwd: 'src/js',
+    //         src: 'app.js',
+    //         dest: 'dist/assets/js',
+    //         ext: '.js'
+    //     }]
+    //   }
+    // },
 
     jshint: {
       options: {
@@ -106,7 +221,7 @@ module.exports = function(grunt) {
       },
       all: [
         'Gruntfile.js',
-        'src/js/app.js'
+        'src/js/main.js'
       ]
     },
 
@@ -117,9 +232,11 @@ module.exports = function(grunt) {
     },
 
     clean: {
-      all: [
-          "dist/"
-      ]  
+      dist: [ 'dist/' ],
+      unminified: [
+        'dist/assets/css/main.css',
+        'dist/assets/js/main.js'
+      ]
     },
 
     copy: {
@@ -132,7 +249,7 @@ module.exports = function(grunt) {
       scripts: {
         expand: true,
         cwd: 'src',
-        src: 'js/app.js',
+        src: 'js/main.js',
         dest: 'dist/assets/',
       },
       images: {
@@ -143,72 +260,91 @@ module.exports = function(grunt) {
       }
     },
 
+    useminPrepare: {
+      html: 'dist/index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+
+    usemin: {
+      options: {
+        dirs: ['dist']
+      },
+      html: ['dist/{,*/}*.html'],
+      css:  ['dist/assets/css/{,*/}*.css']
+    },
+
     watch: {
       options: {
         livereload: true,
       },
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
       scripts: {
         files: ['src/js/**'],
-        tasks: ['copy:scripts', 'jshint'],
+        tasks: ['jshint', 'copy:scripts'],
         options: {
           spawn: false,
         },
       },
       fonts: {
-        files: ['src/font/**'],
+        files: ['src/fonts/**'],
         tasks: ['copy:fonts']
       },
       styles: {
         files: ['src/css/**'],
-        tasks: ['sass:dev']
+        tasks: ['sass:dev', 'autoprefixer:dev']
       },
       liquid: {
         files: ['src/liquid/**'],
-        tasks: ['liquid:dev']
+        tasks: ['liquid:dev', 'wiredep']
       },
       images: {
         files: ['src/img/**'],
-        tasks: ['copy:images']
+        tasks: ['newer:copy:images']
       }
     },
 
   });
 
-  // MODULES
-  grunt.loadNpmTasks('grunt-http-server');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-liquid');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  //grunt.loadNpmTasks('grunt-concurrent');
-  //grunt.loadNpmTasks('grunt-autoprefixer');
 
   // TASKS
+
   grunt.registerTask('server', [
-    'http-server',
+    'connect:livereload',
     'watch'
   ]);
 
   grunt.registerTask('dev', [
-    'clean', 
-    'sass:dev',
     'jshint',
+    'clean:dist',
+    'sass:dev',
+    'autoprefixer:dev',
     'concat',
     'liquid:dev',
+    'wiredep',
     'copy'
   ]);
 
   grunt.registerTask('prod', [
-    'clean', 
-    'sass:prod',
     'jshint',
+    'clean:dist',
+    'sass:prod',
+    'autoprefixer:prod',
     'liquid:prod',
-    'concat',
-    'uglify',
+    'wiredep',
+    // 'concat',
+    // 'uglify',
+    'copy:scripts',
+    'useminPrepare',
+    'concat:generated',
+    'cssmin:generated',
+    'uglify:generated',
+    'usemin',
+    'clean:unminified',
     'copy:fonts',
     'copy:images'
   ]);

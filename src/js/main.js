9f2fc7e2bc1bd95245b8e4540a9d6a773e6f4be9
Michael Rumbesht <m.rumbesht@gmail.com>
2015-08-05 22:12:26 +0100

    Scope main.js up

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,30 @@
-$(document).ready(function(){
+(function( $, window, document, undefined ) {
 
-    var html = $('html');
-    var body = $('body');
-    var winWidth = $(window).width(); 
-    var winHeight = $(window).height(); 
+  'use strict';
 
-    //FastClick init 
+  // var html = $('html');
+  // var body = $('body');
+  // var winWidth = $(window).width();
+  // var winHeight = $(window).height();
+
+
+  function initialize() {
+    console.log('DOM initialized');
+  }
+
+  //attach to the global scope
+  window.finalScripts = {
+    init : initialize
+  };
+
+  //after the DOM is ready call our init function
+  $(function(){
+
+    //FastClick init
     FastClick.attach(document.body);
-});
+
+    finalScripts.init();
+
+  });
+
+})( jQuery, window, document );

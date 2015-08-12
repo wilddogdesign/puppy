08e2495ce227de57eabe579a44790237c3b12056
Misha Rumbesht <misha@wilddogdesign.co.uk>
2015-08-12 17:57:59 +0100

    Start working on styles

--- gruntfile.js
+++ gruntfile.js
@@ -195,6 +195,21 @@ module.exports = function(grunt) {
       }
     },
 
+    svgstore: {
+      options: {
+        prefix : 'icon-', // This will prefix each ID
+        svg: { // will add and overide the the default xmlns="http://www.w3.org/2000/svg" attribute to the resulting SVG
+          viewBox : '0 0 100 100',
+          xmlns: 'http://www.w3.org/2000/svg'
+        }
+      },
+      default: {
+        files: {
+
+        }
+      },
+    },
+
     // Automatic Bower script and styles inclusion
     wiredep: {
 

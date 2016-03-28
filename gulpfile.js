4c8807138a7333080f2c7f23d848b560c815fd62
Michael Rumbesht <m.rumbesht@gmail.com>
2016-03-28 14:13:43 +0100

    Add production routine

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 
 const gulp = require('gulp');
 const del = require('del');
+const gulpif = require('gulp-if');
 const watch = require('gulp-watch');
+const inject = require('gulp-inject');
 const eslint = require('gulp-eslint');
 const data = require('gulp-data');
 const swig = require('gulp-swig');
@@ -18,11 +20,22 @@ const cache = require('gulp-cache');
 const path = require('path');
 const svgmin = require('gulp-svgmin');
 const svgstore = require('gulp-svgstore');
+const prettify = require('gulp-jsbeautifier');
+const rev = require('gulp-rev');
+const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync');
+
+var isDevelopment = process.argv.indexOf("build:production") > -1 ? false : true;
+
+const args = {
+  critical: process.argv.indexOf("--no-critical") > -1 ? false : true,
+  minify:   process.argv.indexOf("--no-minification") > -1 ? false : true
+};
 
 const options = {
   src:  'src',
-  dist: 'dist'
+  dist: 'dist',
+  tmp:  '.tmp'
 };
 
 /* GENERAL */
@@ -31,8 +44,15 @@ gulp.task('cache:clear', cb => cache.clearAll(cb));
 
 gulp.task('clean', () => {
   return del([
-    options.dist
+    options.dist,
+    options.tmp
   ]);
+});
+
+gulp.task('copy:index', () => {
+  return gulp
+    .src(`${options.src}/index/**/*`)
+    .pipe(gulp.dest(options.dist))
 });
 
 gulp.task('fonts', () => {
@@ -69,6 +89,7 @@ gulp.task('modernizr', () => {
         'webworkers'
       ],
     }))
+    .pipe(uglify())
     .pipe(gulp.dest(`${options.dist}/assets/js/vendor`))
 });
 
@@ -99,15 +120,15 @@ gulp.task('styles', () => {
     outputStyle: 'expanded'
   };
   let processors = [
-    autoprefixer({browsers: ['last 2 versions', '> 5%']}),
-    cssnano
+    autoprefixer({browsers: ['last 2 versions', '> 5%']})
   ];
+  if (args.minify) { processors.push(cssnano) }
   return gulp
     .src(`${options.src}/css/main.scss`)
-    .pipe(sourcemaps.init())
+    .pipe(gulpif(isDevelopment,sourcemaps.init()))
       .pipe(sass(sass(sassOptions).on('error', sass.logError)))
       .pipe(postcss(processors))
-    .pipe(sourcemaps.write())
+    .pipe(gulpif(isDevelopment,sourcemaps.write()))
     .pipe(gulp.dest(output));
 });
 
@@ -130,7 +151,21 @@ gulp.task('scripts', gulp.series(
     let output = `${options.dist}/assets/js`;
     return gulp
       .src(`${options.src}/js/**/*.js`)
+      // .pipe(gulpif(isDevelopment,prettify(),uglify()))
       .pipe(gulp.dest(output));
+  })
+);
+
+gulp.task('scripts:production', gulp.series(
+  'lint',
+  () => {
+    let output = `${options.dist}/assets/js`;
+    return gulp
+      .src(`${options.src}/js/**/*.js`)
+      // .pipe(gulpif(isDevelopment,prettify(),uglify()))
+      .pipe(shell([
+        `jspm bundle-sfx ${options.src}/js/main.js ${output}/main.js --skip-source-maps ${args.minify ? '--minify' : ''}`
+      ]));
   })
 );
 
@@ -143,11 +178,17 @@ gulp.task('templates', () => {
     .pipe(data( file => require('./data/pages.json') ))
     .pipe(swig({
       data: {
-        now: new Date()
+        now: new Date(),
+        critical: args.critical,
+        isDevelopment: isDevelopment
       },
       defaults: {
         cache: false
       }
+    }))
+    .pipe(prettify({
+      indent_level: 2,
+      unformatted: ['script']
     }))
     .pipe(gulp.dest(output));
 });
@@ -157,6 +198,7 @@ gulp.task('templates', () => {
 gulp.task('build:dev', gulp.series(
   'clean',
   gulp.parallel(
+    'copy:index',
     'fonts',
     'images',
     'modernizr',
@@ -222,6 +264,117 @@ gulp.task('watch', gulp.parallel(
 
 /* PRODUCTION */
 
+gulp.task('styles:critical', () => {
+  let output = `${options.tmp}/assets/css`;
+  let sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+  };
+  let processors = [
+    autoprefixer({browsers: ['last 2 versions', '> 5%']}),
+    cssnano
+  ];
+  return gulp
+    .src(`${options.src}/css/critical.scss`)
+    .pipe(sass(sass(sassOptions).on('error', sass.logError)))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(output));
+});
+
+gulp.task('inject:critical', () => {
+  return gulp
+    .src(`${options.dist}/**/*.html`)
+    .pipe(inject(gulp.src([`${options.tmp}/assets/css/critical.css`]), {
+      starttag: '<!-- inject:critical -->',
+      transform: (filePath, file) => {
+        // return file contents as string
+        return `<style type="text/css">${file.contents.toString('utf8')}</style>`;
+      }
+    }))
+    .pipe(inject(gulp.src([`${options.dist}/assets/css/*.css`], {read:false}), {
+      starttag: '/* inject:css */',
+      endtag: '/* endinject */',
+      relative: true,
+      transform: (filePath, file) => {
+        // return file contents as string
+        // return file.contents.toString('utf8')
+        return `loadCSS('${filePath.toString('utf8')}', document.getElementById('criticalCss') );`;
+      }
+    }))
+    .pipe(gulp.dest(options.dist));
+});
+
+gulp.task('inject:assets', () => {
+  let target = gulp.src(`${options.dist}/**/*.html`);
+  let css = gulp.src(
+    [
+      `${options.dist}/assets/css/**/*.css`
+    ],
+    {read: false}
+  );
+  let js = gulp.src(
+    [
+      `${options.dist}/assets/js/**/*.js`,
+      `!${options.dist}/assets/js/vendor/modernizr-custom.js`
+    ],
+    {read: false}
+  );
+
+  return target
+    .pipe(inject(css, {relative:true}))
+    .pipe(inject(js, {
+      relative: true,
+      transform: (filepath, file, i, length) => {
+        return `<script src="${filepath}" async></script>`;
+      }
+    }))
+    .pipe(gulp.dest(options.dist));
+});
+
+gulp.task('rev', () => {
+    // by default, gulp would pick `assets/css` as the base,
+    // so we need to set it explicitly:
+    return gulp
+      .src([
+          `${options.dist}/assets/css/main.css`,
+          `${options.dist}/assets/js/main.js`
+        ],
+        {
+          base: `${options.dist}/assets`
+        }
+      )
+      .pipe(gulp.dest(`${options.dist}/assets`))  // copy original assets to build dir
+      .pipe(rev())
+      .pipe(gulp.dest(`${options.dist}/assets`))  // write rev'd assets to build dir
+      // .pipe(rev.manifest())
+      // .pipe(gulp.dest('build/assets')); // write manifest to build dir
+});
+
+gulp.task('clean:unrevved', () => {
+  return del([
+    `${options.dist}/assets/css/main.css`,
+    `${options.dist}/assets/js/main.js`
+  ])
+});
+
+gulp.task('build:production', gulp.series(
+  'clean',
+  gulp.parallel(
+    'copy:index',
+    'fonts',
+    'images',
+    'modernizr',
+    'scripts:production',
+    'styles',
+    'styles:critical',
+    'svgstore',
+    'templates'
+  ),
+  'rev',
+  'clean:unrevved',
+  'inject:critical',
+  'inject:assets'
+));
 
 /* DEFAULT */
 

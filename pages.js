09f58d6b59618a662f8c5fd9a526db8eea531cf1
Tristan Brookes <tristan.s.brookes@gmail.com>
2019-10-24 15:39:02 +0100

    Feat: Move examples to own page. Reason: So you can just get going on the home page.

--- pages.js
+++ pages.js
@@ -1,6 +1,11 @@
 // This stores all the non-index nunjucks pages for generation
 module.exports = [
   {
+    file: 'examples',
+    name: 'Examples',
+    desc: 'Page that shows how to use built in functionality.',
+  },
+  {
     file: 'offline',
     name: 'Offline',
     desc: 'Page a user will see with no active internet connection.',

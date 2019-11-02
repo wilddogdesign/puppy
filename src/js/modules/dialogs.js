7fbe71a5e6c3a1714bb8830d945de5d4b7771a4f
Sean Murrin <seanmurrin@gmail.com>
2019-11-02 15:12:37 +0000

    Chore: updates to dialogs.js

--- src/js/modules/dialogs.js
+++ src/js/modules/dialogs.js
@@ -1,19 +1,25 @@
+// https://github.com/edenspiekermann/a11y-dialog
 import A11yDialog from 'a11y-dialog';
 
 /**
  * Setup dialog components
  *
+ * Functions can be assigned to the onHideCallback and onShowCallback options.
+ *
  * @export
- * @param {*} [options={}] The setup options
+ * @param {*} {
+ *   target = '.js-dialog',
+ *   bodyClass = 'is-visible--dialog',
+ *   onShowCallback = null,
+ *   onHideCallback = null,
+ * }
  */
-export function setupDialogs(options = {}) {
-  const {
-    target = '.js-dialog',
-    bodyClass = 'is-visible--dialog',
-    onShowCallback = null,
-    onHideCallback = null,
-  } = options;
-
+export function setupDialogs({
+  target = '.js-dialog',
+  bodyClass = 'is-visible--dialog',
+  onShowCallback = null,
+  onHideCallback = null,
+} = {}) {
   const dialogs = document.querySelectorAll(target);
 
   Array.from(dialogs).forEach(el => {

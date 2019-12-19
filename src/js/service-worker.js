e4ab06a77ffb42e5710f744355b7fcf87b2878c9
Tristan Brookes <tristan@wilddogdesign.co.uk>
2019-12-19 09:07:41 +0000

    Feat: Update service worker for Wordpress / Webpack Note: There is no offline mode at the moment.

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -1,36 +1,83 @@
-/* eslint-disable */
+/* eslint-disable no-undef */
+/* eslint-disable prefer-arrow-callback */
 
-// This is the "Offline page" service worker
-// Install stage sets up the offline page in the cache and opens a new cache
-self.addEventListener('install', function(event) {
-  var offlinePage = new Request('/offline.html');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-  event.waitUntil(
-    fetch(offlinePage).then(function(response) {
-      return caches.open('pwa-offline').then(function(cache) {
-        console.log('Cached offline page during Install'+ response.url);
-        return cache.put(offlinePage, response);
-      });
-  }));
-});
-
-// If any fetch fails, it will show the offline page.
-// Maybe this should be limited to HTML documents?
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-      console.error( 'Network request Failed. Serving offline page ' + error );
-      return caches.open('pwa-offline').then(function(cache) {
-        return cache.match('/offline.html');
-      });
+if (workbox) {
+  // Skip waiting for service worker update
+  self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      console.log('skip waiting');
+      skipWaiting();
     }
-  ));
-});
-
-// This is a event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener('refreshOffline', function(response) {
-  return caches.open('pwa-offline').then(function(cache) {
-    console.log('Offline page updated from refreshOffline event: '+ response.url);
-    return cache.put(offlinePage, response);
   });
-});
+
+  // no caching on wordpress routes
+  workbox.routing.registerRoute(new RegExp('/wp/'), new workbox.strategies.NetworkOnly());
+
+  // Javascript
+  workbox.routing.registerRoute(
+    /\.js$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'js-cache',
+    }),
+  );
+
+  // CSS
+  workbox.routing.registerRoute(
+    /\.css$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'css-cache',
+    }),
+  );
+
+  // Fonts
+  workbox.routing.registerRoute(
+    /\.(?:ttf|woff|woff2)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'font-cache',
+    }),
+  );
+
+  // Images
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 200,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    }),
+  );
+
+  // Google Analytics
+  workbox.googleAnalytics.initialize();
+
+  // Google Fonts stylesheets with a stale-while-revalidate strategy.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'google-fonts-stylesheets-cache',
+    }),
+  );
+
+  // Cache the underlying font files with a cache-first strategy for 1 year.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'google-fonts-webfonts-cache',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        }),
+      ],
+    }),
+  );
+}
--- webpack.common.js
+++ webpack.common.js
@@ -96,6 +96,7 @@ module.exports = {
                 now: dateFormat(now, 'dd-mm-yyyy @ HH:MM'),
                 pages: [{ file: 'index', name: 'Index' }].concat(otherNunjucksFiles),
                 projectTitle,
+                isProduction: process.argv[process.argv.indexOf('--mode') + 1] === 'production',
               },
             },
           },

f439c6a5eb2a92fbaa5aa5f9fe84c1d422b8eb05
Sean Murrin <seanmurrin@gmail.com>
2020-01-06 16:29:53 +0000

    Chore: recaptcha update

--- src/js/modules/recaptcha.js
+++ src/js/modules/recaptcha.js
@@ -1,41 +1,7 @@
-// https://github.com/jonathantneal/closest - For IE11 support
-// Can be be removed if you don't want to support IE11
-import elementClosest from 'element-closest';
-
-elementClosest(window);
-
 const recaptchaKey = typeof window.recaptchaKey !== 'undefined' ? window.recaptchaKey : '';
 
 /**
- * Get the forms for recaptcha.
- *
- * @export
- * @param {string} [target='.js-recaptcha']
- * @param {boolean} [lookForGRecaptcha=true]
- * @returns
- */
-// export function getForms(target = '.js-recaptcha', lookForGRecaptcha = true) {
-//   const forms = Array.from(document.querySelectorAll(target));
-
-//   // Also look for forms with recaptcha div el
-//   // https://developers.google.com/recaptcha/docs/invisible#programmatic_execute
-//   if (lookForGRecaptcha) {
-//     const recaptchas = document.querySelectorAll('div.g-recaptcha');
-
-//     Array.from(recaptchas).forEach(recaptcha => {
-//       const form = recaptcha.closest('form');
-
-//       if (!form.classList.contains(target.substr(1, target.length))) {
-//         forms.push(form);
-//       }
-//     });
-//   }
-
-//   return forms;
-// }
-
-/**
- *Submit the active recaptcha form.
+ * Submit the active recaptcha form, if it exists.
  *
  * @export
  */
@@ -46,7 +12,7 @@ export function recaptchaSubmit() {
 }
 
 /**
- * Setup Recaptcha on forms
+ * Setup Recaptcha on forms.
  *
  * @export
  * @param {*} [options={}] The setup options
@@ -56,8 +22,9 @@ export function setupRecaptcha({ target = '.js-recaptcha', validate = true } = {
 
   const forms = Array.from(document.querySelectorAll(target));
 
-  Array.from(forms).forEach(form => {
+  forms.forEach(form => {
     const recaptcha = document.createElement('div');
+
     form.appendChild(recaptcha);
 
     // The recaptcha render method returns an id that we can target further down.
@@ -73,7 +40,7 @@ export function setupRecaptcha({ target = '.js-recaptcha', validate = true } = {
       // this could be modified to use a form validation library, but by default uses standard API
       // https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/checkValidity
       if ((validate && form.checkValidity()) || !validate) {
-        // Using the index we can have multiple forms with there own recaptcha.
+        // Using the widgetID we can have multiple forms with there own recaptcha.
         window.grecaptcha.reset(widgetID);
         window.grecaptcha.execute(widgetID);
 

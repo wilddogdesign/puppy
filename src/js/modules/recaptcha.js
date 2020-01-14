64d93f5524afc4bf18c6694c5607840be3db26a7
Sean Murrin <seanmurrin@gmail.com>
2020-01-14 09:58:32 +0000

    Chore: recaptcha updates

--- src/js/modules/recaptcha.js
+++ src/js/modules/recaptcha.js
@@ -23,30 +23,41 @@ export function setupRecaptcha({ target = '.js-recaptcha', validate = true } = {
   const forms = Array.from(document.querySelectorAll(target));
 
   forms.forEach(form => {
-    const recaptcha = document.createElement('div');
-
-    form.appendChild(recaptcha);
-
-    // The recaptcha render method returns an id that we can target further down.
-    const widgetID = window.grecaptcha.render(recaptcha, {
-      sitekey: recaptchaKey,
-      size: 'invisible',
-      callback: recaptchaSubmit,
-    });
-
-    form.addEventListener('submit', ev => {
-      ev.preventDefault();
-
-      // this could be modified to use a form validation library, but by default uses standard API
-      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/checkValidity
-      if ((validate && form.checkValidity()) || !validate) {
-        // Using the widgetID we can have multiple forms with there own recaptcha.
-        window.grecaptcha.reset(widgetID);
-        window.grecaptcha.execute(widgetID);
-
-        window.activeRecaptchaForm = form;
+    if (!window.recaptchaForms.includes(form.id)) {
+      if (!form.id) {
+        // eslint-disable-next-line no-param-reassign
+        form.id = `form-${Math.random()
+          .toString(36)
+          .substr(2, 9)}`;
       }
-    });
+
+      window.recaptchaForms.push(form.id);
+
+      const recaptcha = document.createElement('div');
+
+      form.appendChild(recaptcha);
+
+      // The recaptcha render method returns an id that we can target further down.
+      const widgetID = window.grecaptcha.render(recaptcha, {
+        sitekey: recaptchaKey,
+        size: 'invisible',
+        callback: recaptchaSubmit,
+      });
+
+      form.addEventListener('submit', ev => {
+        ev.preventDefault();
+
+        // this could be modified to use a form validation library, but by default uses standard API
+        // https://developer.mozilla.org/en-US/docs/Web/API/HTMLSelectElement/checkValidity
+        if ((validate && form.checkValidity()) || !validate) {
+          // Using the widgetID we can have multiple forms with there own recaptcha.
+          window.grecaptcha.reset(widgetID);
+          window.grecaptcha.execute(widgetID);
+
+          window.activeRecaptchaForm = form;
+        }
+      });
+    }
   });
 }
 

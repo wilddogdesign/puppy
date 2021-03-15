e7feb5b53dd76ee34b3deeef3a8f0760d7542047
eric-wild-dog <eric@wilddogdesign.co.uk>
2021-03-15 11:18:35 +0000

    WIP: Migrate workbox v4 to v5, cache home, offline.html everything else

--- src/js/service-worker.js
+++ src/js/service-worker.js
@@ -1,25 +1,55 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 /* eslint-disable prefer-arrow-callback */
-const version = '#VERSION_NO#';
+const version = '#VERSION_NO.1.0.12#';
 
 importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+  'https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js'
 );
 
 if (workbox) {
+  console.log('Workbox found, doing the thing..');
+
   // Skip waiting for service worker update
   self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
-      // console.log("skip waiting");
+      console.log('skip waiting');
       skipWaiting();
     }
   });
 
-  // no caching on wordpress routes
+  // cache specific pages as HTML for offline fallback
   workbox.routing.registerRoute(
-    new RegExp('/wp/'),
-    new workbox.strategies.NetworkOnly()
+    function matchFunction({ url }) {
+      const pages = ['/', '/offline.html'];
+      return pages.includes(url.pathname);
+    },
+    new workbox.strategies.NetworkFirst({
+      // with these pages..
+      // always try Network First, fallback to Cache for offline
+      cacheName: 'html-cache',
+    })
+  );
+
+  const networkOnly = new workbox.strategies.NetworkOnly();
+  const navigationHandler = async (params) => {
+    try {
+      // Attempt a network request.
+      // if we've cached this resource seperately (eg. home or offline.html or CSS, or JS)
+      // then this will not fail
+      return await networkOnly.handle(params);
+    } catch (error) {
+      // If it fails, return the cached HTML for offline.html
+      // eg. pages that aren't home or seperately cached
+      return caches.match('/offline.html', {
+        cacheName: 'html-cache',
+      });
+    }
+  };
+
+  // Register this strategy to handle all navigations.
+  workbox.routing.registerRoute(
+    new workbox.routing.NavigationRoute(navigationHandler)
   );
 
   // Javascript
@@ -52,7 +82,7 @@ if (workbox) {
     new workbox.strategies.CacheFirst({
       cacheName: 'image-cache',
       plugins: [
-        new workbox.expiration.Plugin({
+        new workbox.expiration.ExpirationPlugin({
           maxEntries: 200,
           maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
         }),
@@ -77,10 +107,10 @@ if (workbox) {
     new workbox.strategies.CacheFirst({
       cacheName: 'google-fonts-webfonts-cache',
       plugins: [
-        new workbox.cacheableResponse.Plugin({
+        new workbox.cacheableResponse.CacheableResponsePlugin({
           statuses: [0, 200],
         }),
-        new workbox.expiration.Plugin({
+        new workbox.expiration.ExpirationPlugin({
           maxAgeSeconds: 60 * 60 * 24 * 365,
           maxEntries: 30,
         }),
